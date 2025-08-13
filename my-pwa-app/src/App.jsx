28f69c909751c58157be9404f0b9436153a95436
Devindi N karunathilaka <devindikarunathilaka88@gmail.com>
2025-08-13 22:57:38 +0530

    Create settup of PWA app

---
+++ my-pwa-app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Hello PWA 👋</h1>
+      <p>This works offline and can be installed on your device.</p>
+    </div>
+  )
+}
